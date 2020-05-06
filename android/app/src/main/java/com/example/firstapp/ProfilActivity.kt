4f058028bc2ee9ecc27b506a4f9e4a8220a09232
andikadarma <darmaandika58@gmail.com>
2020-05-06 23:50:31 +0700

    project init

---
+++ android/app/src/main/java/com/example/firstapp/ProfilActivity.kt
@@ -0,0 +1,12 @@
+package com.example.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
